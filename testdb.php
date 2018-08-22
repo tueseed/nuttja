f11282aca23bffe16d45c8e6368867e465ba61c9
tueseed <tue_seed@hotmail.com>
2018-08-22 10:57:31 +0700

    Update testdb.php

--- testdb.php
+++ testdb.php
@@ -1,6 +1,6 @@
 <?php
 require('./db/connect-db.php');//เรียกใช้ไฟล์
-$sql_office = "SELECT * FROM tbl_office WHERE office_name LIKE '%เชียง%'";
+$sql_office = "SELECT * FROM tbl_office WHERE office_name LIKE '%กฟย.%'";
 $query_office = mysqli_query($conn,$sql_office);
 while($obj = mysqli_fetch_array($query_office))
 {
