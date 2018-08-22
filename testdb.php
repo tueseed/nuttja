cc585d7eff49efe2fae97ff34a0ab0acf4a739e5
tueseed <tue_seed@hotmail.com>
2018-08-22 12:20:21 +0700

    Add files via upload

--- testdb.php
+++ testdb.php
@@ -1,10 +1,9 @@
 <?php
 require('./db/connect-db.php');//เรียกใช้ไฟล์
-$sql_office = "SELECT * FROM tbl_office WHERE office_name LIKE '%เชียง%' ";
+$sql_office = "SELECT * FROM tdd01.3_report";
 $query_office = mysqli_query($conn,$sql_office);
-<a href="https://drive.google.com/file/d/0B4BTAaYsG0CpRFhQXzFxcjl1SVE/">รูปภาพ</a>
 while($obj = mysqli_fetch_array($query_office))
 {
-	echo "ชื่อการไฟฟ้า ".$obj["office_name"]." รหัสการไฟฟ้า ".$obj["office"]."<br>";
+	echo "การไฟฟ้า ".$obj["area"]." WBS ".$obj["wbs"]."<br>";
 }
 ?>
