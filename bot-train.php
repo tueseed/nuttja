7681d7199648f9499241b65091bd7b2a80760047
tueseed <tue_seed@hotmail.com>
2018-08-22 11:52:21 +0700

    Add files via upload

--- bot-train.php
+++ bot-train.php
@@ -38,10 +38,10 @@ if (!is_null($events['events'])) //check ค่าในตัวแปร $even
 			if($first_char == "@")
 			{
 				$office_id = substr($txtin,1,3);///ได้รหัสการไฟฟ้า 
-				$sql_area = "SELECT * FROM tdd01.3_report WHERE Area = '$office_id'";
+				$sql_area = "SELECT * FROM tdd01.3_report WHERE area = '$office_id'";
 				$query_area = mysqli_query($conn,$sql_area);
 				$num_row = mysqli_num_rows($query_area);// นับจำนวนที่หาเจอ
-				reply_msg($office_id,$replyToken);//เรียกใช้ function
+				reply_msg($num_row,$replyToken);//เรียกใช้ function
 				break;
 			}
          /*ลงทะเบียนกลุ่ม*/   
