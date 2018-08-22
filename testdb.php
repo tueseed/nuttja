7a05d047c62c635fc2476a33ee078abd64897795
tueseed <tue_seed@hotmail.com>
2018-08-22 12:28:36 +0700

    Add files via upload

--- testdb.php
+++ testdb.php
@@ -1,9 +1,9 @@
 <?php
 require('./db/connect-db.php');//เรียกใช้ไฟล์
-$sql_office = "SELECT * FROM tdd01.3_report";
+$sql_office = "SELECT * FROM tbl_office";
 $query_office = mysqli_query($conn,$sql_office);
 while($obj = mysqli_fetch_array($query_office))
 {
-	echo "การไฟฟ้า ".$obj["area"]." WBS ".$obj["wbs"]."<br>";
+	echo "การไฟฟ้า ".$obj["office_name"]." WBS ".$obj["office"]."<br>";
 }
 ?>
