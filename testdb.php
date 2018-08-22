3ce07ae9abfe893f3e980772496c63148503f68e
tueseed <tue_seed@hotmail.com>
2018-08-22 10:06:01 +0700

    Add files via upload

---
+++ testdb.php
@@ -0,0 +1,9 @@
+<?php
+require('./db/connect-db.php');//เรียกใช้ไฟล์
+$sql_office = "SELECT * FROM tbl_office";
+$query_office = mysqli_query($conn,$sql_office);
+while($obj = mysqli_fetch_array($query_office))
+{
+	echo $obj["office_name"]."<br>";
+}
+?>
